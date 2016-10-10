77e480838e816cc766d4d43524e4f5bc9b5ecefa
Methodician <jacobgjohnston@gmail.com>
2016-10-10 03:57:42 -0700

    Started the basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,28 @@ import { AngularFire, FirebaseObjectObservable, FirebaseListObservable, AuthProv
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   items: FirebaseListObservable<any[]>;
+  
+  c1: any = {};
+  c2: any = {};
+
+  c1R: number;
+  c1G: number;
+  c1B: number;
+
+  c2R: number;
+  c2G: number;
+  c2B: number;
+
   constructor(public af: AngularFire) {
     this.items = af.database.list('items');
+  }
+
+  updateC1(){
+    document.getElementById('colorBox1').style.backgroundColor = 'rgb(' + this.c1.R + ',' + this.c1.G + ',' + this.c1.B + ')';
+    //let cb = document.getElementById('colorBox1');
+    //cb.style.backgroundColor = 'rgb(' + this.c1.R + ',' + this.c1.G + ',' + this.c1.B + ')';
   }
 
   loginGoogle(){
