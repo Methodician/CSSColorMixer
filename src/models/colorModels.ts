038fbf96d0c5dd7f1f6d5fd6a168fe0c7b4815ce
Methodician <jacobgjohnston@gmail.com>
2016-10-18 09:46:25 -0700

    Added hex codes translation from rgb

--- src/models/colorModels.ts
+++ src/models/colorModels.ts
@@ -3,11 +3,16 @@ export class rgbColor {
     g: number;
     b: number;
     rgb: string;
+    hex: string;
     constructor(r: number, g: number, b: number){
         this.r = r;
         this.g = g;
         this.b = b;
+        let hexR = r==0? '00' : r.toString(16)
+        let hexG = g==0? '00' : g.toString(16)
+        let hexB = b==0? '00' : b.toString(16)
         this.rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
+        this.hex = ('#' + hexR + hexG + hexB).toUpperCase();
     }
 }
 
@@ -16,4 +21,5 @@ export interface IrgbColor{
     g: number;
     b: number;
     rgb: string;
+    hex: string;
 }
