51a86f0dc41d0e92836d030437b3bfa9336023ac
Methodician <jacobgjohnston@gmail.com>
2016-10-21 05:06:31 -0700

    Add, Subtract, Mix, and mix+others working

--- src/app/palette/colourPalette.comp.ts
+++ src/app/palette/colourPalette.comp.ts
@@ -12,18 +12,22 @@ import { rgbColor, IrgbColor } from '../../models/colorModels';
 export class ColourPaletteComp implements OnInit {
 
   //items: FirebaseListObservable<any[]>;
-  method: string = 'Avg';
+  method: string = 'Average';
+  averageOn = true;
+  addOn = false;
+  minusOn = false;
   poolSet = false;
+  showHex = true;
 
-  red: IrgbColor = new rgbColor(255,0,0);
+  red: IrgbColor = new rgbColor(255, 0, 0);
   //yellow: IrgbColor = new rgbColor(255,255,0);
-  green: IrgbColor = new rgbColor(0,255,0);
-  blue: IrgbColor = new rgbColor(0,0,255);
-  white: IrgbColor = new rgbColor(255,255,255);
-  black: IrgbColor = new rgbColor(0,0,0);
+  green: IrgbColor = new rgbColor(0, 255, 0);
+  blue: IrgbColor = new rgbColor(0, 0, 255);
+  white: IrgbColor = new rgbColor(255, 255, 255);
+  black: IrgbColor = new rgbColor(0, 0, 0);
 
 
-  paletteColor: IrgbColor = new rgbColor(127,127,127);
+  poolColor: IrgbColor = new rgbColor(127, 127, 127);
 
   colors: IrgbColor[] = [];
 
@@ -31,124 +35,154 @@ export class ColourPaletteComp implements OnInit {
     //this.items = af.database.list('items');
   }
 
-  ngOnInit(){
+  ngOnInit() {
     //this.setRgbPicks();
+    this.initializeColors();
+    this.setElementColor('colorPool', this.poolColor);
+  }
+
+  clearColors() {
     this.initializeColors();
   }
 
-  initializeColors(){
+  toggleHex() {
+    this.showHex = !this.showHex;
+  }
+
+  initializeColors() {
+    this.colors = [];
     this.colors.push(this.red);
     //this.colors.push(this.yellow);
     this.colors.push(this.green);
     this.colors.push(this.blue);
     this.colors.push(this.white);
     this.colors.push(this.black);
-    this.setElementColor('colorPool', this.paletteColor);
+
   }
-/*  
-  setRgbPicks(){
-    this.setElementColor('redPick', this.red);
-    this.setElementColor('greenPick', this.green);
-    this.setElementColor('bluePick', this.blue);
-    this.setElementColor('colorPool', this.paletteColor);
-  }
-*/
-  setElementColor(elementId: string, color: IrgbColor){
+
+  setElementColor(elementId: string, color: IrgbColor) {
     document.getElementById(elementId)
       .style.backgroundColor = color.rgb;
   }
 
-  pickColor(color: IrgbColor){
-    if(!this.poolSet){
+  pickColor(color: IrgbColor) {
+    if (!this.poolSet) {
       this.setElementColor('colorPool', color);
-      this.paletteColor = color;
+      this.poolColor = color;
       this.poolSet = true;
     }
-    else{
+    else {
       var newColor: IrgbColor
-      if(this.method == 'Avg')
-        newColor = this.mixColors(color, this.paletteColor);
-      else newColor = this.addColors(color, this.paletteColor);
+      if (this.averageOn && !this.addOn && !this.minusOn)
+        newColor = this.mixColors(this.poolColor, color);
+      else if (this.addOn)
+        newColor = this.addColors(this.poolColor, color);
+      else if (this.minusOn)
+        newColor = this.subtractColors(this.poolColor, color);
       this.setElementColor('colorPool', newColor);
-      this.paletteColor = newColor;
+      this.poolColor = newColor;
     }
 
   }
 
-
-  saveColor(){
-    this.colors.push(this.paletteColor);
+  subtractColors(c1: IrgbColor, c2: IrgbColor): IrgbColor {
+    let r = this.subtract(c1.r, this.averageOn ? this.average(c1.r, c2.r) : c2.r));
+    let g = this.subtract(c1.g, this.averageOn ? this.average(c1.g, c2.g) : c2.g));
+    let b = this.subtract(c1.b, this.averageOn ? this.average(c1.b, c2.b) : c2.b);
+    return new rgbColor(r, g, b);
+  }
+  subtract(a: number, b: number) {
+    var combo = a;
+    if (a < b)
+      combo = b - a;
+    if (b < a)
+      combo = a - b;
+    else
+      return 0;
+    return combo;
   }
 
-  resetPool(){
-    let freshPool = new rgbColor(127,127,127);
-    this.paletteColor = freshPool;
-    this.setElementColor('colorPool', freshPool);
-    this.poolSet = false;
+  addColors(c1: IrgbColor, c2: IrgbColor): IrgbColor {
+    let r = this.combine(c1.r, this.averageOn ? this.average(c1.r, c2.r) : c2.r);
+    let g = this.combine(c1.g, this.averageOn ? this.average(c1.g, c2.g) : c2.g);
+    let b = this.combine(c1.b, this.averageOn ? this.average(c1.b, c2.b) : c2.b);
+    return new rgbColor(r, g, b);
   }
-
-  addColors(c1: IrgbColor, c2: IrgbColor): IrgbColor{
-    let r = this.combine(c1.r, c2.r);
-    let g = this.combine(c1.g, c2.g);
-    let b = this.combine(c1.b, c2.b);
-    return new rgbColor(r,g,b);
-  }
-
-  combine(a: number, b: number){
+  combine(a: number, b: number) {
     var combo = a + b;
-    if(combo > 255)
+    if (combo > 255)
       return 255;
     else return combo;
   }
 
-  mixColors(c1: IrgbColor, c2: IrgbColor): IrgbColor{
+  mixColors(c1: IrgbColor, c2: IrgbColor): IrgbColor {
     let r = this.average(c1.r, c2.r);
     let g = this.average(c1.g, c2.g);
     let b = this.average(c1.b, c2.b);
-    return new rgbColor(r,g,b);
+    return new rgbColor(r, g, b);
   }
 
-  average(a: number, b: number){
-    return Math.round( ( a + b ) /2 );
+  average(a: number, b: number) {
+    return Math.round((a + b) / 2);
   }
 
-  setAvg(){
-    this.method = 'Avg';
+  toggleAvg() {
+    this.method = 'Average';
+    this.averageOn = !this.averageOn;
   }
-  setAdd(){
+  toggleAdd() {
     this.method = 'Add';
+    this.addOn = !this.addOn;
+    if (this.addOn)
+      this.minusOn = false;
+  }
+  toggleMinus() {
+    this.method = 'Minus';
+    this.minusOn = !this.minusOn;
+    if (this.minusOn)
+      this.addOn = false;
   }
 
-/*  updateC1(){
-    document.getElementById('colorBox1')
-      .style.backgroundColor =
-      'rgb(' + this.c1.R + ',' + this.c1.G + ',' + this.c1.B + ')';
+  saveColor() {
+    this.colors.push(this.poolColor);
+  }
 
-      this.mixColors();
-    //let cb = document.getElementById('colorBox1');
-    //cb.style.backgroundColor = 'rgb(' + this.c1.R + ',' + this.c1.G + ',' + this.c1.B + ')';
-  }*/
+  resetPool() {
+    let freshPool = new rgbColor(127, 127, 127);
+    this.poolColor = freshPool;
+    this.setElementColor('colorPool', freshPool);
+    this.poolSet = false;
+  }
+  /*  updateC1(){
+      document.getElementById('colorBox1')
+        .style.backgroundColor =
+        'rgb(' + this.c1.R + ',' + this.c1.G + ',' + this.c1.B + ')';
+  
+        this.mixColors();
+      //let cb = document.getElementById('colorBox1');
+      //cb.style.backgroundColor = 'rgb(' + this.c1.R + ',' + this.c1.G + ',' + this.c1.B + ')';
+    }*/
 
-/*  updateC2(){
-    document.getElementById('colorBox2')
-      .style.backgroundColor =
-      'rgb(' + this.c2.R + ',' + this.c2.G + ',' + this.c2.B + ')';
+  /*  updateC2(){
+      document.getElementById('colorBox2')
+        .style.backgroundColor =
+        'rgb(' + this.c2.R + ',' + this.c2.G + ',' + this.c2.B + ')';
+  
+        this.mixColors();
+      //let cb = document.getElementById('colorBox1');
+      //cb.style.backgroundColor = 'rgb(' + this.c1.R + ',' + this.c1.G + ',' + this.c1.B + ')';
+    }*/
 
-      this.mixColors();
-    //let cb = document.getElementById('colorBox1');
-    //cb.style.backgroundColor = 'rgb(' + this.c1.R + ',' + this.c1.G + ',' + this.c1.B + ')';
-  }*/
-
-/*  mixColors(){
-    let cR = Math.round(( this.c1.R + this.c2.R ) / 2);
-    let cG = Math.round(( this.c1.G + this.c2.G ) / 2);
-    let cB = Math.round(( this.c1.B + this.c2.B ) / 2);
-    this.mixedColor.R = cR;
-    this.mixedColor.G = cG;
-    this.mixedColor.B = cB;
-    document.getElementById('mixedColorBox')
-      .style.backgroundColor =
-      'rgb(' + cR + ',' + cG + ',' + cB + ')';
-  }*/
+  /*  mixColors(){
+      let cR = Math.round(( this.c1.R + this.c2.R ) / 2);
+      let cG = Math.round(( this.c1.G + this.c2.G ) / 2);
+      let cB = Math.round(( this.c1.B + this.c2.B ) / 2);
+      this.mixedColor.R = cR;
+      this.mixedColor.G = cG;
+      this.mixedColor.B = cB;
+      document.getElementById('mixedColorBox')
+        .style.backgroundColor =
+        'rgb(' + cR + ',' + cG + ',' + cB + ')';
+    }*/
 
 }
